f312942d499314b3d94ef2a0ddcef331ad6ebb8e
Andrew Capp <acapp909@gmail.com>
2020-01-10 00:55:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  require_relative 'config/environment'
+  class App < Sinatra::Base
+    get '/reversename/:name' do
+      params[:name].reverse
+    end
+    get '/square/:number' do
+      (params[:number].to_i * params[:number].to_i).to_s
+    end
+    get '/say/:number/:phrase' do
+      string = ""
+      params[:number].to_i.times do
+        string << params[:phrase]
+      end
+      string
+    end
+  end
 end
